5b267534d359f8872c1d6d0d4984b46d34fecfcb
aziza <azizamakhkamova88@gmail.com>
2026-05-21 15:25:38 -0600

    all players list created

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
 // === Constants ===
-const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
-const API = BASE + COHORT;
+const BASE = 'https://fsa-puppy-bowl.herokuapp.com/api';
+const COHORT = '/2603-FTB-CT-WEB-PT'; // Make sure to change this!
+const RESOURCE = '/players';
+const API = BASE + COHORT + RESOURCE;
+
+console.log('API', API);
+
+// STATE
+let players = [];
+let selectedPlayer = null;
+
+async function getAllPlayers() {
+  try {
+    const response = await fetch(API);
+    const result = await response.json();
+    players = result.data.players;
+    render();
+  } catch (error) {
+    console.error('Error fetching all players', error);
+  }
+}
+
+// COMPONENTS
+function PlayerCard(player) {
+  const $card = document.createElement('article');
+  $card.classList.add('player');
+  $card.innerHTML = `
+  <img src="${player.imageUrl}" alt="${player.name}"/>
+  <h2>${player.name}</h2>
+  `;
+  $card.addEventListener('click', function () {
+    selectedPlayer = player;
+    render();
+  });
+  return $card;
+}
+
+function PlayerCollection() {
+  const $section = document.createElement('section');
+  $section.classList.add('players');
+
+  const playerCards = players.map(PlayerCard);
+  $section.replaceChildren(...playerCards);
+  return $section;
+}
+
+// RENDER
+function render() {
+  const $app = document.querySelector('#app');
+
+  $app.innerHTML = `
+  <h1>Players</h1>
+  <PlayerCollection></PlayerCollection>`;
+
+  $app.querySelector('PlayerCollection').replaceWith(PlayerCollection());
+}
+async function init() {
+  await getAllPlayers();
+  // render();
+}
+
+init();
+
+// render();
+
+// getAllPlayers();
